2efe5c8d0f82b6e6b67f0e389abca6fdee71068f
Wajid Hussain <wajid.husayn@gmail.com>
2020-02-01 23:21:36 +0500

    uppercase lower case program

---
+++ W3Schools/src/UpperLower.java
@@ -0,0 +1,12 @@
+
+public class UpperLower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String txt = "Hello World";
+System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+System.out.println(txt.toLowerCase());   // Outputs "hello world"
+	}
+
+}
