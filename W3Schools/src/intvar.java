e6f736c15d8cf8109661a1717018ca9089a53a81
Wajid Hussain <wajid.husayn@gmail.com>
2020-02-01 23:17:51 +0500

    new programs added

---
+++ W3Schools/src/intvar.java
@@ -0,0 +1,11 @@
+
+public class intvar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 6;
+		System.out.println(x + y); // Print the value of x + y
+	}
+
+}
---
+++ W3Schools/src/stering.java
@@ -0,0 +1,10 @@
+
+public class stering {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		System.out.println("The length of the txt string is: " + txt.length());
+	}
+
+}
