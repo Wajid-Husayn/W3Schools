f3ab1a4467a5be16be20b08816ad709a3aa966c9
Wajid Hussain <wajid.husayn@gmail.com>
2020-02-01 23:33:20 +0500

    concatenate

---
+++ W3Schools/src/strConcat.java
@@ -0,0 +1,12 @@
+
+public class strConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String firstName = "John";
+String lastName = "Doe";
+System.out.println(firstName + " " + lastName);
+	}
+
+}
