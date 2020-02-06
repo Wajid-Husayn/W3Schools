06fe43452d2cc37a03ff6e519a73a7cc1e48e459
Wajid Hussain <wajid.husayn@gmail.com>
2020-02-06 16:51:22 +0500

    added oop programs

---
+++ W3Schools/src/Oops/StaticNestedClass.java
@@ -0,0 +1,19 @@
+package Oops;
+//Static Inner Class
+//An inner class can also be static, which means that you can access it without creating an object of the outer class:
+class OuterClass1 {
+	  int x = 10;
+
+	  static class InnerClass {
+	    int y = 5;
+	  }
+	}
+
+	public class StaticNestedClass {
+	  public static void main(String[] args) {
+	    OuterClass1.InnerClass myInner = new OuterClass1.InnerClass();
+	    System.out.println(myInner.y);
+	  }
+	}
+
+	// Outputs 5
