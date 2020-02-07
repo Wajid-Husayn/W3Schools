bcc84039cbeb8ac147f4189b1b31f7f0c88a9237
Wajid-Husayn <45106498+Wajid-Husayn@users.noreply.github.com>
2020-02-07 11:13:00 +0500

    Update HelloWorld.java
    added Comment

--- W3Schools/src/HelloWorld.java
+++ W3Schools/src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 
 			  public static void main(String[] args) {
-			    System.out.println("Hello World");
+			    System.out.println("Hello World"); //This line will print Hello world
 			    System.out.println("Hello Pakistan");
 			  }
 			
